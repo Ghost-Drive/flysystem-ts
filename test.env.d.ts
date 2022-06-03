75c28bd3266fef486879c8157a0cf3069ac081d1
nik-selecto <n.moiseienko@selectoglobal.com>
2022-06-03 13:13:23 +0300

    refactor(package.json): husky hooks

---
+++ test.env.d.ts
@@ -0,0 +1,10 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        // TODO create .test.env file with your secrets for testing porposes
+        // TODO add their type declarations to this interface
+      }
+    }
+  }
+
+export { };
