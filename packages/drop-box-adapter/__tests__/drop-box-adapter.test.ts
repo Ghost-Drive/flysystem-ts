0ec0cab9cd562025d85f3d01338444eda181f359
nik-selecto <n.moiseienko@selectoglobal.com>
2022-06-06 12:00:19 +0300

    fix(test): fix test's name in drop-box-adapter package

--- packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
+++ packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
@@ -1,4 +1,4 @@
-describe('Common package testing', () => {
+describe('Drop-box-adapter package testing', () => {
     const errorMessage = 'Any tests found!';
 
     it(errorMessage, () => console.error(errorMessage));
