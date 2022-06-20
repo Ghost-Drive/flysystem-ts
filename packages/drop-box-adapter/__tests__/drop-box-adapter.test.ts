a712196cba222ac3d6c7dfa10c7df5ec8773c81c
Nikita Moiseienko <nikitamoiseienko@Nikitas-MacBook-Pro.local>
2022-06-21 00:55:23 +0300

    test(dropb-box): some tests

--- packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
+++ packages/drop-box-adapter/__tests__/drop-box-adapter.test.ts
@@ -1,9 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { config } from 'dotenv';
 import { Flysystem } from '@flysystem-ts/flysystem';
+import * as fs from 'fs';
+import { join } from 'path';
 import { DropboxAdapter } from '../src/index';
 
 config({ path: '../../../.test.env' });
+
+const TEST_PIC_NAME = 'photo-for-test.jpg';
+const TEST_PIC_PATH = join(__dirname, '../resourses', TEST_PIC_NAME);
 
 describe('Drop-box-adapter package testing', () => {
     let flysystem: Flysystem<DropboxAdapter>;
@@ -26,5 +31,39 @@ describe('Drop-box-adapter package testing', () => {
         const res = await flysystem.directoryExists('no-such-directory');
 
         expect(res).toBe(false);
+    });
+
+    it('Should create and remove directory', async () => {
+        await flysystem.createDirectory('hello');
+
+        const isFolderCreated = await flysystem.directoryExists('hello');
+
+        expect(isFolderCreated).toBe(true);
+        await flysystem.deleteDirectory('hello');
+
+        const isFolderRemoved = !(await flysystem.directoryExists('hello'));
+
+        expect(isFolderRemoved).toBe(true);
+    });
+
+    it('Should upload file', async () => {
+        const pic = fs.readFileSync(TEST_PIC_PATH);
+        const dbxPath = `one/pic-${new Date().getTime()}.jpg`;
+
+        await flysystem.write(dbxPath, pic);
+
+        const success = await flysystem.fileExists(dbxPath);
+
+        expect(success).toBe(true);
+    });
+
+    it('Should upload and remove file', async () => {
+        const pic = fs.readFileSync(TEST_PIC_PATH);
+        const dbxPath = `one/pic-${new Date().getTime()}.jpg`;
+
+        await flysystem.write(dbxPath, pic);
+        await flysystem.delete(dbxPath);
+
+        expect(await flysystem.fileExists(dbxPath)).toBe(false);
     });
 });
